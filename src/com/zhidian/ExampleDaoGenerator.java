20dfeaec2b698493863f38b24150a2d898690040
unknown <lihongbo2011@gamil.com>
2014-05-14 16:48:53 +0800

    init

---
+++ src/com/zhidian/ExampleDaoGenerator.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2011 Markus Junginger, greenrobot (http://greenrobot.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zhidian;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
+
+/**
+ * Generates entities and DAOs for the example project DaoExample.
+ * <p/>
+ * Run it as a Java application (not Android).
+ *
+ * @author Markus
+ */
+public class ExampleDaoGenerator {
+
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.youle.gamebox.ui.greendao");
+        addEntity(schema);
+
+//        new DaoGenerator().generateAll(schema, "D:\\github\\greenDAO\\DaoExample\\src-gen");
+        new DaoGenerator().generateAll(schema, "..\\Youle\\src");
+    }
+
+    private static void addEntity(Schema schema) {
+        Entity entity = schema.addEntity("RecommendGame");
+        entity.addLongProperty("appid").primaryKey();
+        entity.addStringProperty("name");
+        entity.addStringProperty("downloadurl") ;
+        entity.addStringProperty("imageurl") ;
+        entity.addBooleanProperty("isrecommend");
+
+        Entity jsonEntity = schema.addEntity("JsonEntry") ;
+        jsonEntity.addLongProperty("resouce").primaryKey() ;
+        jsonEntity.addStringProperty("json") ;
+    }
+
+
+}
